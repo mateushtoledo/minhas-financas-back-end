4bea28cd697cf1fa3b7a36c8e054e51a235f3489
Mateus Henrique Toledo <mateushtoledo@gmail.com>
2020-05-13 20:08:38 -0300

    Adicionando teste faltante. Otimizando carregamento de contexto nos testes de repositórios JPA

--- src/test/java/com/toledo/minhasfinancas/repository/UserRepositoryTest.java
+++ src/test/java/com/toledo/minhasfinancas/repository/UserRepositoryTest.java
@@ -5,15 +5,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.toledo.minhasfinancas.domain.User;
 
-@SpringBootTest
+@DataJpaTest
 @ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 public class UserRepositoryTest {
 	@Autowired
 	UserRepository repository;
@@ -32,5 +35,17 @@ public class UserRepositoryTest {
 		
 		// verification
 		assertThat(robervalExists).isTrue();
+	}
+	
+	@Test
+	public void testUserNotExistsByEmail() {
+		// Scenario
+		repository.deleteAll();
+				
+		// action
+		boolean robervalExists = repository.existsByEmail("mateushtoledo@gmail.com");
+		
+		// verification
+		assertThat(robervalExists).isFalse();
 	}
 }
